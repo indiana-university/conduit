33c3f71c13957b3f3342f14ffbf200c4561d6de1
Chris Basham <csbasham@iu.edu>
2019-05-16 16:35:41 -0400

    Clean deps

--- examples/word-count/webpack.config.js
+++ examples/word-count/webpack.config.js
@@ -9,10 +9,7 @@ module.exports = {
   },
   externals: {
     'conduit-rxjs': 'Conduit',
-    'conduit-rxjs-react': 'ConduitReact',
     'lighterhtml': 'lighterhtml',
-    'react': 'React',
-    'react-dom': 'ReactDOM',
     'rxjs': 'rxjs',
     'rxjs/operators': 'rxjs.operators',
     'when-elements': 'WhenElements'
